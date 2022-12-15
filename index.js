88097457a7356f22072db6af4a44ad9c4c3aab71
MajdQumseya <qumseya.majd@gmail.com>
2022-12-15 13:40:05 +0200

    added dynamic grid using flex

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let gameContainer = document.querySelector('.game-container')
+let smallGridBtn = document.querySelector('#small-grid');
+let mediumGridBtn = document.querySelector('#medium-grid');
+let largeGridBtn = document.querySelector('#large-grid');
 
+let gridCellDimensions = '55';
 
-function createGrid() {
-    const gridDimensions = 16 //16 x 16
+function createGrid(gridDimensions) {
+    // const gridDimensions = 16 //16 x 16
     const totalBlocks = gridDimensions*gridDimensions
     console.log(gridDimensions)
     for (let i = 0; i < totalBlocks; i++) {
         let gridCell = document.createElement('div')
+        gridCell.style.height = `${gridCellDimensions}px`
+        gridCell.style.width = `${gridCellDimensions}px`
         gridCell.classList.add('grid-cell')
         gridCell.addEventListener('mouseleave', ()=> {
             if(gridCell.classList.contains('background-black')){
@@ -19,4 +25,26 @@ function createGrid() {
     }
 }
 
-createGrid()
+function resetGrid() {
+    gameContainer.innerHTML = '';
+}
+
+createGrid(16)
+
+smallGridBtn.addEventListener('click', () => {
+    resetGrid()
+    gridCellDimensions = '55'
+    createGrid(16)
+})
+
+mediumGridBtn.addEventListener('click', () => {
+    resetGrid()
+    gridCellDimensions = '28'
+    createGrid(32)
+})
+
+largeGridBtn.addEventListener('click', () => {
+    resetGrid()
+    gridCellDimensions = '13'
+    createGrid(64)
+})
